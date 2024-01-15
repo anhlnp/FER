78f961773931ee0eebce41bfafbe20cc089a5a59
anhlnp <anhphucpro13@gmail.com>
2024-01-15 10:27:38 +0700

    basic components

--- fe/lab_02/src/index.js
+++ fe/lab_02/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import HomePage from './pages/users/homePage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <h1> Lab_02</h1>
-);
+root.render(<HomePage/>);
---
+++ fe/lab_02/src/pages/users/homePage/index.js
@@ -0,0 +1,7 @@
+import { memo } from "react";
+
+const HomePage = () => {
+   return <h1> Lab_02 from homePage</h1>;
+};
+
+export default memo(HomePage);
